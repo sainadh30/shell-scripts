d1fde4dfe8881d6ae7b83bfb4885771c569e6e94
sainadh30 <sainadh3009@gmail.com>
2023-11-27 02:17:54 +0000

    Delete & Backup Old Files

---
+++ delete_old_files/deleteoldfile.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Author : Loka Sainadh
+# Date : 26-11-2023
+# Description : This script deletes the old file
+# Modified : 26-11-2023
+
+location="/home/ec2-user/shell-scripts/delete_old_files"
+
+find $location -mtime +90 -exec rm {} \;
